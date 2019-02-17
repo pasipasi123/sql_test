8c6dbbc8d5550de61e90e7fc691327fa95824eb9
pasipasi123 <pasi.haapakorva@gmail.com>
2019-02-17 20:16:11 +0200

    rownames_to_column

--- R/00_memory_db.R
+++ R/00_memory_db.R
@@ -19,7 +19,7 @@ dbcon <- dbConnect(RSQLite::SQLite(), ":memory:")
 
 #' Lisätään kantaan `mtcars`-taulu.
 
-dbWriteTable(dbcon, "mtcars", mtcars %>% as_tibble(rownames = "name"))
+dbWriteTable(dbcon, "mtcars", mtcars %>% rownames_to_column("name"))
 
 dbListTables(dbcon)
 
